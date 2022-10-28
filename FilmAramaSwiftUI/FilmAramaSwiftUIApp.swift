1dc35477cbae163e5d4650f0687e6632fade030e
muhammed gül <muhammedgul.mail@gmail.com>
2022-10-28 11:58:30 +0300

    Initial Commit

---
+++ FilmAramaSwiftUI/FilmAramaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAramaSwiftUIApp.swift
+//  FilmAramaSwiftUI
+//
+//  Created by Muhammed Gül on 28.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FilmAramaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
